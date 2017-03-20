1ad416ca527db391df7e0465737158ee0697ecd3
igork <igork@kotkovetss-Mac-mini.local>
2017-03-20 15:14:19 +0300

    Integrate swiftlint alalyzer

---
+++ LoadingView/Classes/LoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingView.swift
+//  Pods
+//
+//  Created by igork on 3/20/17.
+//
+//
+
+import UIKit
+
+class LoadingView: UIView {
+    var viewComputations: ViewComputations?
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+
+}
---
+++ LoadingView/Classes/ViewComputations.swift
@@ -0,0 +1,13 @@
+//
+//  ViewComputations.swift
+//  Pods
+//
+//  Created by igork on 3/20/17.
+//
+//
+
+import Foundation
+
+class ViewComputations {
+    
+}
